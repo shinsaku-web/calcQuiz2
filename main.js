86f79f4199899d2b8d6184c197210d9c714e04ac
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2023-03-12 20:33:22 +0900

    問題の作成まで

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-alert("こんにちは");
+function createQuestion() {
+  const firstNumber = Math.ceil(Math.random() * 10);
+  const secondNumber = Math.ceil(Math.random() * 10);
+  const calcSignList = ["+", "-", "×", "÷"];
+  const calcSign =
+    calcSignList[Math.floor(Math.random() * calcSignList.length)];
+
+  const getResult = () => {
+    if (calcSign === calcSignList[0]) {
+      return firstNumber + secondNumber;
+    }
+    if (calcSign === calcSignList[1]) {
+      return firstNumber - secondNumber;
+    }
+    if (calcSign === calcSignList[2]) {
+      return firstNumber * secondNumber;
+    }
+    if (calcSign === calcSignList[3]) {
+      return firstNumber / secondNumber;
+    }
+  };
+  const result = getResult();
+  return {
+    number1: firstNumber,
+    number2: secondNumber,
+    sign: calcSign,
+    answer: result,
+  };
+}
+
+const question = createQuestion();
+console.log(question);
